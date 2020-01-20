744ad569b02574d6c49547d17e61974a4baf8489
Alina Shaykhutdinova <alinashay@icloud.com>
2020-01-20 15:15:07 +0300

    add tabs to challenge

--- src/components/Challenge.jsx
+++ src/components/Challenge.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import NavBar from "./Navbar";
 import {tryExit} from "../actions/actions";
 import './Check.scss';
+import {Tabs, TabLink, TabContent} from "react-tabs-redux"
 
 
 
@@ -11,15 +12,12 @@ class Challenge extends React.Component {
     constructor(props) {
         super(props);
         this.exit = this.exit.bind(this);
+        this.getMonthChallenge = this.getMonthChallenge.bind(this);
+        this.state = {data: {m0120: [1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}}
     }
 
     componentDidMount() {
-        Date.prototype.daysInMonth = function() {
-
-          return 32 - new Date(this.getFullYear(), this.getMonth(), 32).getDate();
-        };
-        console.log(new Date().getDate());
-        console.log(new Date().daysInMonth());
+       // this.setState({data: {month: [1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}})
     }
 
     exit() {
@@ -27,29 +25,37 @@ class Challenge extends React.Component {
         this.props.history.push('/');
     }
 
-    render(){
-        Date.prototype.daysInMonth = function() {
-
-            return 32 - new Date(this.getFullYear(), this.getMonth(), 32).getDate();
-        };
+   getMonthChallenge(monthName, yearName) {
         let month = '';
-        for (let i=1; i<=new Date().getDate(); i++)
-        {
+        let daysInMonth = new Date('01.10.2020').getMonth();//32 - new Date(this.getFullYear(), this.getMonth(), 32).getDate();
 
-            if(i%7===0)
-                month+=`<input type="checkbox" id="${i}"/> <label for="${i}" class="check-box">  </label> <br/>`
-                //month+=`<div class="day"> <label for="${i}"> Day ${i}</label> <br/> <input type="radio" id="${i}" name="${i}" value="${i}"> </div> <br/>`;
+        for (let i = 1; i <= new Date().getDate(); i++) {
+            if (i % 7 === 0)
+                if (this.state.data.m0120[i] == 1)
+                    month += `<input type="checkbox" id="${i}${monthName}${yearName}" checked/> <label for="${i}${monthName}${yearName}" class="check-box" >  </label> <br/>`
+                else
+                    month += `<input type="checkbox" id="${i}${monthName}${yearName}"/> <label for="${i}${monthName}${yearName}" class="check-box">  </label> <br/>`
+
+            else if (this.state.data.m0120[i] == 1)
+                month += `<input type="checkbox" id="${i}${monthName}${yearName}" checked/> <label for="${i}" class="check-box">  </label>`;
             else
-                month+=`<input type="checkbox" id="${i}"/> <label for="${i}" class="check-box">  </label>`;
+                month += `<input type="checkbox" id="${i}${monthName}${yearName}"/> <label for="${i}${monthName}${yearName}" class="check-box">  </label>`;
         }
 
-        for (let i=new Date().getDate()+1; i<=new Date().daysInMonth(); i++){
-            if(i%7===0)
-                month+=`<input type="checkbox" id="${i}" disabled="true"/> <label for="${i}" class="check-box"> </label> <br/>`;
+        for (let i = new Date().getDate() + 1; i <= daysInMonth; i++) {
+            if (i % 7 === 0)
+                month += `<input type="checkbox" id="${i}${monthName}${yearName}" disabled="true"/> <label for="${i}${monthName}${yearName}" class="check-box"> </label> <br/>`;
             else
-                month+=`<input type="checkbox" id="${i}" disabled="true"/> <label for="${i}" class="check-box"> </label> </div>`;
+                month += `<input type="checkbox" id="${i}${monthName}${yearName}" disabled="true"/> <label for="${i}${monthName}${yearName}" class="check-box"> </label> </div>`;
         }
+        return <div className="Container" dangerouslySetInnerHTML={{__html:
+           month}}/>
+    }
 
+    render(){
+
+            /* <div className="Container" dangerouslySetInnerHTML={{__html:
+            month}}/>*/
         return(
             <div className="personalAccount">
                 <NavBar/>
@@ -59,12 +65,25 @@ class Challenge extends React.Component {
             </div>
         <div className="Challenge">
             <h2> Challenge </h2>
+                <Tabs>
+                    <TabLink to="tab1">January</TabLink>
+                    <TabLink to="tab2">February</TabLink>
+                    <TabLink to="tab3">March</TabLink>
 
-            <div className="Container" dangerouslySetInnerHTML={{__html:
-                month}}>
+                        <TabContent for="tab1">
+                            {this.getMonthChallenge(1,20)}
+                        </TabContent>
+
+                    <TabContent for="tab2">
+                        {this.getMonthChallenge(2,20)}
+                    </TabContent>
+
+                    <TabContent for="tab3">
+                        {this.getMonthChallenge(3,20)}
+                    </TabContent>
+                </Tabs>
+
             </div>
-
-        </div>
             </div>
         )
     }
